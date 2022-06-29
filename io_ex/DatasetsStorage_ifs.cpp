51e1c5e6d7a4bda416bb1bb52e76e0491994efbe
nicholay <ryazaner@gmail.com>
2022-06-29 18:40:14 +0300

    fix io_ex

--- io_ex/DatasetsStorage_ifs.cpp
+++ io_ex/DatasetsStorage_ifs.cpp
@@ -1,4 +1,4 @@
-
+﻿
 
 
 #include <QFileInfo>
@@ -15,7 +15,12 @@ std::unique_ptr<DatasetsStorage_ifs> openDatasetsStorage(const std::string& file
     std::unique_ptr<DatasetsStorage_ifs> ds_storage;
 
     QFileInfo file_info(filename.c_str());
-    if (file_info.suffix() == "h5") ds_storage = std::make_unique<Hdf5IoInterface>();
+
+    if (file_info.suffix() == "h5"){
+    #ifdef HDF5_IO_INTERFACE_ENABLE
+        ds_storage = std::make_unique<Hdf5IoInterface>();
+    #endif
+    }
     else ds_storage = std::make_unique<FolderIoInterface>();
 
     if (ds_storage->open(filename.c_str(), rw)) return ds_storage;

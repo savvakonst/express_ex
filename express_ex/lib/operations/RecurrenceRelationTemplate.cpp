ed88dc35864950bd9bf5c1bfcbb378e9303d8250
svk <savvakonst@gmail.com>
2022-07-22 04:44:52 +0300

    neighbor poit development

--- express_ex/lib/operations/RecurrenceRelationTemplate.cpp
+++ express_ex/lib/operations/RecurrenceRelationTemplate.cpp
@@ -15,32 +15,41 @@ ExValue_ifs *newRecurrenceRelationTemplate(GarbageContainer *garbage_container, 
     return nullptr;
 }
 
-RecurrenceRelationTemplate::RecurrenceRelationTemplate(ExValue_ifs *ret_value) {
-    stack<ExValue_ifs *> visitor_stack;
-    visitor_stack.push(ret_value->getAssignedVal());
-    do {
-        auto var = visitor_stack.pop();
-        var->reverseTraversalVisitEnter(&visitor_stack);
-        if (var->getNodeType() == NodeTypeEn::kRecursiveNeighborPoint) {
-            // var->
-        }
-    } while (!visitor_stack.empty());
-}
+RecurrenceRelationTemplate::RecurrenceRelationTemplate(ExValue_ifs *ret_value) {}
 
 void RecurrenceRelationTemplate::visitEnterSetupBuffer(stack<ExValue_ifs *> *visitor_stack) {
     Operation_ifs::visitEnterSetupBuffer(visitor_stack);
 }
 
 void RecurrenceRelationTemplate::visitEnterStackUpdate(stack<ExValue_ifs *> *visitor_stack) {
-    //Operation_ifs::visitEnterStackUpdate(visitor_stack);
+    // Operation_ifs::visitEnterStackUpdate(visitor_stack);
 }
 
 void RecurrenceRelationTemplate::genBodyVisitExit(BodyGenContext *context) {
-    //Operation_ifs::genBodyVisitExit(context);
+    // Operation_ifs::genBodyVisitExit(context);
+
+    stack<ExValue_ifs *> path_stack;
+    stack<ExValue_ifs *> temp_stack;
+    stack<ExValue_ifs *> visitor_stack;
+
+    visitor_stack.push(operand_[0]->getAssignedVal());
+    do {
+        auto var = visitor_stack.pop();
+        if (var->getNodeType() == NodeTypeEn::kRecursiveNeighborPoint) {
+            // var->
+        }
+
+        var->reverseTraversalVisitEnter(&temp_stack);
+        path_stack.push_back(temp_stack.front());
+        temp_stack.pop_front();
+        visitor_stack.emplace_back(temp_stack);
+
+
+    } while (!visitor_stack.empty());
 }
 
 void RecurrenceRelationTemplate::calculateConstRecursive(RecursiveGenContext *context) {
-    //Operation_ifs::calculateConstRecursive(context);
+    // Operation_ifs::calculateConstRecursive(context);
 }
 
 void RecurrenceRelationTemplate::printVisitExit(PrintBodyContext *context) { Operation_ifs::printVisitExit(context); }
@@ -50,5 +59,5 @@ std::string RecurrenceRelationTemplate::printUint() { return Operation_ifs::prin
 void RecurrenceRelationTemplate::setupIR(IRGenerator &builder) { Operation_ifs::setupIR(builder); }
 
 void RecurrenceRelationTemplate::calculate() {
-    //Operation_ifs::calculate();
+    // Operation_ifs::calculate();
 }
